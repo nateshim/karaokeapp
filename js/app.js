8af8cc604db9fb7741a25dffa01de17c48d94dad
Nathanael Shim <nateshim@berkeley.edu>
2020-03-30 14:08:52 -0700

    added no input case

--- js/app.js
+++ js/app.js
@@ -2,33 +2,30 @@ function tplawesome(e,t){res=e;for(var n=0;n<t.length;n++){res=res.replace(/\{\{
 
 $(function() {
     $("form").on("submit", function(e) {
-       e.preventDefault();
-       // prepare the request
-       var request = gapi.client.youtube.search.list({
-            part: "snippet",
-            type: "video",
-            q: encodeURIComponent($("#search").val()).replace(/%20/g, "+") + " karaoke",
-            maxResults: 1,
-            order: "relevance"
-       }); 
-       // execute the request
-       request.execute(function(response) {
-          var results = response.result;
-          console.log(results);
-          if (results == " karaoke") {
-            $("#results").html("");
-            $.each(results.items, function(index, item) {
+        e.preventDefault();
+        // prepare the request
+        if ($("#search").val() != "") {
+            var request = gapi.client.youtube.search.list({
+                part: "snippet",
+                type: "video",
+                q: encodeURIComponent($("#search").val()).replace(/%20/g, "+") + " karaoke",
+                maxResults: 1,
+                order: "relevance"
+            }); 
+            // execute the request
+            request.execute(function(response) {
+                var results = response.result;
+                console.log(results);  
+                $("#results").html("");
+                $.each(results.items, function(index, item) {
                 $.get("tpl/item.html", function(data) {
                     $("#results").append(tplawesome(data, [{"title":item.snippet.title, "videoid":item.id.videoId}]));
+                    });
                 });
+                resetVideoHeight();
             });
-            resetVideoHeight();
-            }
-          } 
-            
-       );
+        }
     });
-    
     $(window).on("resize", resetVideoHeight);
 });
 
